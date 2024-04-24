815adaac032ca1bcd1592c896d81277bc837778f
Arthur Ferreira <fepi.arthur@gmail.com>
2024-04-24 10:26:13 -0300

    Enhance main application to demonstrate creation of cars, bikes, and trucks

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"factory-method-go/pkg/factory"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Factory Method Pattern in Go")
-	// Placeholder for factory method demonstration
+
+	// Create a car using the CarFactory
+	carFactory := factory.NewCarFactory()
+	car := carFactory.CreateVehicle()
+	fmt.Println("Created Vehicle - Car:", car.Details())
+
+	// Create a bike using the BikeFactory
+	bikeFactory := factory.NewBikeFactory()
+	bike := bikeFactory.CreateVehicle()
+	fmt.Println("Created Vehicle - Bike:", bike.Details())
+
+	// Create a truck using the TruckFactory
+	truckFactory := factory.NewTruckFactory()
+	truck := truckFactory.CreateVehicle()
+	fmt.Println("Created Vehicle - Truck:", truck.Details())
 }
